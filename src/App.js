251cd55e57bc5c0924a37d39af7093423cfb245c
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2023-01-19 12:30:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
+      <Home />
       
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<div>live page</div>} />
-          <Route path="/home" element={<div>home page</div>} />
-          <Route path="/contact" element={<div>contact page</div>} />
+        
+          <Route path="/trynowcreated/" element={ <Home />} />
+          <Route path="/trynowcreated/About" element={ <About/>} />
+          <Route path="/trynowcreated/contact" element={<div>contact page</div>} />
+          <Route path="/" element={<div>contact page</div>} />
+          
         </Routes>
       </BrowserRouter>
     </div>
