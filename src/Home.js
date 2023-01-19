8b25786c71bcc94a30067e44ecced537547a120d
Ankit kushwaha <119472022+Ankitkushwaha90@users.noreply.github.com>
2023-01-19 12:33:38 +0530

    Create Home.js

---
+++ src/Home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () =>{
+        return <h1>hello add created web <a href="/trynowcreated/">hello</a><a href="/trynowcreated/contact">Contact</a><a href="/trynowcreated/home">Home</a></h1>;
+};
+
+export default Home;
